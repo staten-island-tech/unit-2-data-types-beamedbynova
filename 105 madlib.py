e96de1a6377f81f6481fd6486f2fa2e69f1c3006
beamedbynova <novatherelone@gmail.com>
2024-09-19 07:47:44 -0400

    please do this please
    eoibsiognoig

--- 105 madlib.py
+++ 105 madlib.py
@@ -1,5 +1,5 @@
 # Madlib story:
-# I'm sure you've heard the [adjective] story of George Washington and the [Noun] Tree. If you haven't, let me remind you. 
+# I'm sure you've heard the great story of George Washington and the [Noun] Tree. If you haven't, let me remind you. 
 # When Washington was a young [noun], maybe around [number] year / years of age, his [adjective] father gifted him a very sharp [noun].
 # "What a sharp [noun]!" Washington exclaimed excitedly. Washington's father was happy to see his son enjoying his gift.
 # Washington was so excited to test out his brand new [noun], he walked right out of his [noun] and into the surrounding wilderness without telling his father anything!
@@ -15,15 +15,20 @@
 
 quizStart = input("You are about to take a quiz on one of the oldest myths in American history. Type 'Yes' to continue. ")
 if quizStart == "Yes":
-    disclaimer = input("Disclaimer: the answers in this quiz do not have to be accurate. There are no 'wrong' answers when it comes to this quiz, so you can answer with anything you want. Type 'I understand' to continue. ")
+    disclaimer = input("Disclaimer: the answers in this quiz do not have to be accurate. There are no 'wrong' answers when it comes to this quiz, so you may answer however you want. Type 'I understand' to continue. ")
     if disclaimer == "I understand":
         tree = input("George Washington was the first president of the United States. There is a popular myth surrounding him related to a tree; what was the tree featured in the myth? (respond with a noun) ")
-        washingtonYoung = input("What is Washington? (respond with a noun) ")
+        washingtonSpecies = input("What is Washington? (respond with a noun) ")
         washingtonAge = input("How old is Washington in the story? ")
         washingtonFather = input("How would you describe Washington's father? (respond with an abstract noun) ")
-        washingtonGift = input("In the story, Washington's father gave him a gift. What was the gift? (respond with a noun) ")
+        washingtonGift = input("In the story, Washington's father gives him a gift. What was the gift? (respond with a noun) ")
         washingtonHouse = input("What does Washington live inside of? (respond with a noun) ")
-        
+        storyConfirm = input("The quiz is now over. Type 'Proceed' to see the retelling of 'George Washington and the " + tree + " tree,' according to your answered. ")
+        if storyConfirm == "Proceed":
+            print("When Washington was a young " + washingtonSpecies + " (about " + washingtonAge + " years old), his " + washingtonFather + " father gifted him a very sharp " + washingtonGift + ".")
+            print("'Wow! What a sharp " + washingtonGift + "!' Washington exclaims, stunned as he looks at the gift in his hands.")
+        else:
+            print("You have not typed 'Proceed' correctly. Please try again by running the code.")
     else:
         print("You have not agreed to the terms.")
 else:
