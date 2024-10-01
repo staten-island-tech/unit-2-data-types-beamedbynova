727f491402beaaec62b20f67b17c9e1d8da254ec
beamedbynova (Noah Destajo) <novatherelone@gmail.com>
2024-10-01 09:19:25 -0400

    something idk

--- 109 challenge6.py
+++ 109 challenge6.py
@@ -1,14 +1,12 @@
 # this is challenge 6 / a function that finds the greatest common factor of two numbers
 
-a = int(input("Input your first number: "))
-b = int(input("Input your second number: "))
-listOfNumbers = []
+a = int(input("Input your first number: ")) # first number
+b = int(input("Input your second number: ")) # second number
+listOfNumbers = [] # list that takes all of the common factors
 
-def greatestCommonFactor(x, y):
-    for i in range(1, b + 1):
-        if a % i == 0 and b % i == 0:
-            listOfNumbers.append(i)
-    print(listOfNumbers[-1])
-greatestCommonFactor(a,b)
-
-# 
+def greatestCommonFactor(x, y): # defines function to find gcf
+    for i in range(1, b + 1): # for loop that has every number from 1 to b
+        if x % i == 0 and y % i == 0: # if statement that requires x and y to both be 0 after a modulo
+            listOfNumbers.append(i) # if both x and y are 0, its added to the list
+    print(max(listOfNumbers)) # prints the last number in the list, the gcf
+greatestCommonFactor(a,b) # calls function
--- testing.py
+++ testing.py
@@ -1,21 +1,8 @@
-variable = 5
-if variable == 5:
-    variable2 = 14
-    print(variable2)
+a = int(input("Input your first number: "))
+b = int(input("Input your second number: "))
 
-bill = 100
-portionOfBill = bill / 100 * 15
-
-print(portionOfBill)
-
-
-# this code first asks for a number.
-# it then creates a loop with a condition.
-# if the number is greater than zero, the loop will print the number, and then subtract 1 from the number.
-# it will continue to do this until the number no longer fufills the condition (if the number is greater than zero)
-number = int(input("please input a number: "))
-while number > 0:
-    print(number)
-    number = number - 1
-
-type(14) == int
+def greatestCommonFactor(x, y):
+    for i in range(1, b + 1):
+        if x % i == 0 and y % i == 0:
+            print(i)
+greatestCommonFactor(a,b)
