8ac8c0e1ffa3f740ef785abb5005675083600ba9
beamedbynova (Noah Destajo) <novatherelone@gmail.com>
2024-10-07 08:47:09 -0400

    e

--- testing.py
+++ testing.py
@@ -1,8 +1,6 @@
-a = int(input("Input your first number: "))
-b = int(input("Input your second number: "))
+x = 3
 
-def greatestCommonFactor(x, y):
-    for i in range(1, b + 1):
-        if x % i == 0 and y % i == 0:
-            print(i)
-greatestCommonFactor(a,b)
+for i in range(x,1):
+    print(i)
+
+# if the first value in a for loop is larger than the second, nothing is printed out
